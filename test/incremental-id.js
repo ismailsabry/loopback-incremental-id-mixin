947b9bcb3ba4218ed06e4e3d3647476a200f5f0f
Loïc Mahieu <mahieuloic@gmail.com>
2016-04-11 18:37:55 +0200

    Model.update

--- test/incremental-id.js
+++ test/incremental-id.js
@@ -35,43 +35,55 @@ const createBook = (options, data) => {
 }
 
 describe('Incremental ID', _ => {
-  describe('Via internal API', _ => {
-    describe('Model.create', _ => {
-      it('should begin at 1 if no existing record', () => {
-        return expect(createBook()).to.eventually.equal(1)
-      })
-      it('should begin at `start` option', () => {
-        return expect(createBook({
-          start: 99
-        })).to.eventually.equal(99)
-      })
-      it('should not use `increment` option if no existing record', () => {
-        return expect(createBook({
-          increment: v => 50
-        })).to.eventually.equal(1)
-      })
-      it('should increment 1 of the latest record', () => {
-        const Book = createModel()
+  describe('Model.create', _ => {
+    it('should begin at 1 if no existing record', () => {
+      return expect(createBook()).to.eventually.equal(1)
+    })
+    it('should begin at `start` option', () => {
+      return expect(createBook({
+        start: 99
+      })).to.eventually.equal(99)
+    })
+    it('should not use `increment` option if no existing record', () => {
+      return expect(createBook({
+        increment: v => 50
+      })).to.eventually.equal(1)
+    })
+    it('should increment 1 of the latest record', () => {
+      const Book = createModel()
 
-        return expect(
-          Book.create({ name: 'foo' })
-            .then(_ => Book.create({ name: 'foo' }))
-            .then(_ => Book.create({ name: 'foo' }))
-            .then(record => record.id)
-        ).to.eventually.equal(3)
+      return expect(
+        Book.create({ name: 'foo' })
+          .then(_ => Book.create({ name: 'foo' }))
+          .then(_ => Book.create({ name: 'foo' }))
+          .then(record => record.id)
+      ).to.eventually.equal(3)
+    })
+    it('should use `increment` option', () => {
+      const Book = createModel({
+        increment: v => parseInt(v, 10) + 10
       })
-      it('should use `increment` option', () => {
-        const Book = createModel({
-          increment: v => parseInt(v, 10) + 10
-        })
 
-        return expect(
-          Book.create({ name: 'foo' })
-            .then(_ => Book.create({ name: 'foo' }))
-            .then(_ => Book.create({ name: 'foo' }))
-            .then(record => record.id)
-        ).to.eventually.equal(21)
-      })
+      return expect(
+        Book.create({ name: 'foo' })
+          .then(_ => Book.create({ name: 'foo' }))
+          .then(_ => Book.create({ name: 'foo' }))
+          .then(record => record.id)
+      ).to.eventually.equal(21)
+    })
+  })
+  describe('Model.update', _ => {
+    it('should not use the mixin', () => {
+      const Book = createModel()
+
+      return expect(
+        Book.create({ name: 'foo' })
+          .then(record => {
+            record.name = 'bar'
+            return record.save()
+          })
+          .then(record => record.id)
+      ).to.eventually.equal(1)
     })
   })
 })
